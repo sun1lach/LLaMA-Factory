b86a017ee99111bb1fcf88230b0e6c72618500a6
sun1lach <sunil.acharya@intel.com>
2024-05-05 20:15:29 +0530

    Updated predict.py

--- predict.py
+++ predict.py
@@ -1,6 +1,23 @@
+import os
+
+for e in os.environ:
+    print(e, " -> ", os.environ[e])
+
+import subprocess
+
+try:
+    subprocess.check_call(["pip", "install", "."])
+    subprocess.check_call(["pip", "freeze"])
+
+except subprocess.CalledProcessError as e:
+    print(f"Error running - {e}")
+
+
 from llmtuner.api.app import run_api
+
 
 def predict(data):
     return f"In predict function - {data}"
+
 
 run_api()
