721cc56828be13e946a1dc6de27d310c10ebb5a4
sun1lach <sunil.acharya@intel.com>
2024-05-06 07:28:29 +0530

    Added deploy task

--- predict.py
+++ predict.py
@@ -1,23 +1,37 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import os
 
-for e in os.environ:
-    print(e, " -> ", os.environ[e])
+# device = "cpu"  # the device to load the model onto
 
-# import subprocess
+model_path = os.path.join(os.getenv("INPUT_MERGE", "/input/merge"), "putput", "merged")
+print(model_path)
 
-# try:
-#     subprocess.check_call(["pip", "install", "."])
-#     subprocess.check_call(["pip", "freeze"])
-
-# except subprocess.CalledProcessError as e:
-#     print(f"Error running - {e}")
+model = AutoModelForCausalLM.from_pretrained(
+    model_path, torch_dtype="auto", trust_remote_code=True
+)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 
-# from llmtuner.api.app import run_api
+def chat(prompt):
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt},
+    ]
+    text = tokenizer.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt")
+
+    generated_ids = model.generate(model_inputs.input_ids, max_new_tokens=512)
+    generated_ids = [
+        output_ids[len(input_ids) :]
+        for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    return response
 
 
-def predict(data):
-    return f"In predict function - {data}"
-
-
-# run_api()
+if __name__ == "__main__":
+    print("Inside Main")
